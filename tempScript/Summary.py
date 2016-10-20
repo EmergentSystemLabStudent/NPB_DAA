d2651e98fb6fdb6db873b9bd86fd81810f68b455
yuuki Tada <is0220ii@ed.ristumei.ac.jp>
2016-10-20 13:07:38 +0900

    Change SummaryScript

--- tempScript/Summary.py
+++ tempScript/Summary.py
@@ -1,12 +1,45 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 from sklearn import metrics
 import matplotlib
 matplotlib.use('Agg') #in the case of perform on server
-import matplotlib.pyplot as plt
-import json, os, pickle, csv, argparse, multiprocessing, time
-from paver.easy import pushd
+import pickle
+import json
 #-------------------------------
 
 class Summary(object):
     def __init__(self, dirpath = '.'):
-        print "test"
+        print(dirpath)
+
+        with open(dirpath+'/'+'parameter.json') as f:
+            self.params = json.load(f)
+        with open(dirpath+'/'+'fig_title.json') as f2:
+            self.fig_title = json.load(f2)
+        with open(dirpath+'/'+'sample_word_list.txt') as f3:
+            self.word_list = pickle.load(f3)
+        self.data_size = self.params['DATA_N']
+        print (self.data_size)
+        self.input_data = [np.loadtxt("./LABEL/" + i + ".lab") for i in self.fig_title]#label change here
+        self.input_data2 = [np.loadtxt("./LABEL/" + i + ".lab2") for i in self.fig_title]#label change here
+        self.sample_states = [np.loadtxt(dirpath+'/'+'sample_states_%d.txt' % i) for i in range(self.params['DATA_N'])]
+        self.sample_letters = [np.loadtxt(dirpath+'/'+'sample_letters_%d.txt' % i) for i in range(self.params['DATA_N'])] #%iをループで記述している．
+        self.state_ranges = []
+
+        for i in range(self.params['DATA_N']):#ファイル読み込み
+            with open('state_ranges_%d.txt' % i) as f:
+                self.state_ranges.append(pickle.load(f))
+        llist = np.loadtxt("loglikelihood.txt").tolist()
+        print llist
+        self.maxlikelihood = (max(llist), llist.index(max(llist)))
+        # manipulation part ラベル保管用
+        self.l_label_dic = {}
+        self.s_label_dic = {}
+        # manipulation part end
+        # --------------------------------------letter&state confused matrix function--------------------------------------#
+        def letter_confused_matrix(self):
+            #change here
+            a = []
+            i = []
+            for key, key2 in zip(self.sample_letters, self.input_data):
+
+        # --------------------------------------write result_graph--------------------------------------#
--- tempScript/parser.py
+++ tempScript/parser.py
@@ -3,7 +3,7 @@
 import numpy as np
 import argparse,os,sys
 import Summary
-
+from paver.easy import pushd
 ##parse する
 #=====================summary　class=====================#
 
@@ -12,23 +12,26 @@ import Summary
 
 def main():
     parser = argparse.ArgumentParser(description='DATA of NPB_DAA analysis script')
-    parser.add_argument('-d',help='SummaryData of NPB_DAA')
+    parser.add_argument('-d',dest='resultdata',help='SummaryData of NPB_DAA')
     #parser.add_argument('-r', required=True) # このオプションは必須です
     parser.add_argument('--version', action='version', version='%(prog)s 1.0') # version
     args = parser.parse_args() # コマンドラインの引数を解釈します
-    #print(args)
+    print(args.resultdata)
     figs_dir = 'summary_figs'
     if(os.path.exists(figs_dir)==True):
-        print("summary_figs exited.Can I overwrite ?")
+        print(figs_dir+" exited.Can I overwrite ?")
         var = raw_input("y or n? >")
         if var =='n':
             print("this program finish")
             sys.exit()
         os.rmdir(figs_dir)
     os.mkdir(figs_dir)
-    test = Summary.Summary()
-    #evaluation_result save#
-
+    summary = Summary.Summary("./"+args.resultdata)
+    #evaluation_result save
+    with pushd(figs_dir):
+            #gen confused matrix
+            summary.letter_confused_matrix()
+            summary.state_confused_matrix()
 
 if __name__ == '__main__':
     main()

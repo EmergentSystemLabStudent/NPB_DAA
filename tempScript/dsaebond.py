1de70ecfa38235cf7295a0b7008ca334a16225c5
yota2013 <pdpaaj@gmail.com>
2016-11-21 16:09:24 +0900

    Dsae script make

---
+++ tempScript/dsaebond.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib
+matplotlib.use('tkagg')
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+def main():
+    print  os.listdir()
+
+    #ファイルを読み込んで
+    #ある結合ファイルに保管
+
+
+
+if __name__ == '__main__':
+    main()
